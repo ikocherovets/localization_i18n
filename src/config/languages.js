98e7736b43941554007b1a1abbf72a9af5f78948
Illia Kocherovets <illiakocherovets@gmail.com>
2024-09-23 01:48:17 +0200

    make adjustments

---
+++ src/config/languages.js
@@ -0,0 +1,18 @@
+export const languages = [
+  {
+    code: "fr",
+    name: "Français",
+    country_code: "FR", // ISO 3166-1 alpha-2 country code
+  },
+  {
+    code: "en",
+    name: "English",
+    country_code: "GB", // ISO 3166-1 alpha-2 country code
+  },
+  {
+    code: "ar",
+    name: "عربي",
+    country_code: "SA", // ISO 3166-1 alpha-2 country code
+    dir: "rtl",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,34 +1,12 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import HttpApi from "i18next-http-backend";
-import "bootstrap/dist/js/bootstrap.js";
 
+import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import './utils/i18n';
 import "./index.css";
 import App from "./App";
-
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .use(LanguageDetector)
-  .use(HttpApi)
-  .init({
-    // the translations
-    // (tip move them in a JSON file and import them,
-    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    supportedLngs: ["en", "fr", "ar"],
-    fallbackLng: "en",
-    detection: {
-      order: ["path", "cookie", "htmlTag", "localStorage", "subdomain"],
-      caches: ["cookie"],
-    },
-    backend: {
-      loadPath: "/assets/locales/{{lng}}/translation.json",
-    },
-  });
 
 const loadingMarkup = (
   <div class="py-4 text-center">
